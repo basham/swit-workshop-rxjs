6927b982b3fea0f1a6c9e4ff12c951f51da98017
Chris Basham <csbasham@iu.edu>
2019-09-05 16:09:22 -0400

    Simplify implementation

--- src/util.js
+++ src/util.js
@@ -3,26 +3,21 @@ import { combineLatest, isObservable, of } from 'rxjs'
 import { map, tap } from 'rxjs/operators'
 
 export const combineLatestProps = (source) => {
-  const keys = Object.keys(source)
-  const streamKeys = keys
+  const streamKeys = Object.keys(source)
     .filter((key) => isObservable(source[key]))
   if (!streamKeys.length) {
     return of(source)
   }
   const streams = streamKeys
     .map((key) => source[key])
-  const data = keys
-    .filter((key) => !isObservable(source[key]))
-    .reduce((out, key) => ({ ...out, [key]: source[key] }), {})
   return combineLatest(streams).pipe(
     map((values) =>
       values
-        .map((value, i) => ({ [streamKeys[i]]: value }))
-        .reduce((out, value) => ({ ...out, ...value }), data)
-    ),
-    map((props) =>
-      keys
-        .reduce((out, key) => ({ ...out, [key]: props[key] }), {})
+        .reduce((out, value, i) => {
+          const key = streamKeys[i]
+          out[key] = value
+          return out
+        }, { ...source })
     )
   )
 }
