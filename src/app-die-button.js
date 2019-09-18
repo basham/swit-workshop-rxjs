fc42811a324892a2d418a64e3d0c273682eea361
Chris Basham <csbasham@iu.edu>
2019-09-18 17:40:08 -0400

    Remove click handler

--- src/app-die-button.js
+++ src/app-die-button.js
@@ -1,13 +1,12 @@
 import { html } from 'lighterhtml'
 import { map } from 'rxjs/operators'
 import { whenAdded } from 'when-elements'
-import { adoptStyles, combineLatestProps, fromAttribute, fromProperty, renderComponent } from './util.js'
+import { adoptStyles, combineLatestProps, fromAttribute, renderComponent } from './util.js'
 import css from './app-die-button.css'
 
 adoptStyles(css)
 
 whenAdded('app-die-button', (el) => {
-  const click$ = fromProperty(el, 'click')
   const description$ = fromAttribute(el, 'description')
   const faces$ = fromAttribute(el, 'faces').pipe(
     map((value) => parseInt(value) || 6)
@@ -15,7 +14,6 @@ whenAdded('app-die-button', (el) => {
   const label$ = fromAttribute(el, 'label')
 
   const renderSub = combineLatestProps({
-    click: click$,
     description: description$,
     label: label$,
     faces: faces$
@@ -28,14 +26,13 @@ whenAdded('app-die-button', (el) => {
   }
 
   function render (props) {
-    const { click, description, faces, label } = props
+    const { description, faces, label } = props
     const type = `d${faces}`
     const icon = `dice.svg#${type}`
     return html`
       <button
         aria-label=${description}
-        class='button'
-        onclick=${click}>
+        class='button'>
         <svg class='icon'>
           <use xlink:href=${icon} />
         </svg>
