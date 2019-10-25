8f5730efb1a1a8d6a422c2a76110c8d207d39ce8
Chris Basham <chris@bash.am>
2019-10-25 17:29:40 -0400

    Add initial dice-root code to example

--- exercises/01-dice-button/dice-root.js
+++ exercises/01-dice-button/dice-root.js
@@ -1,1 +1,25 @@
-// dice-root.js
+import { define, html, render } from '/lib/util/dom.js'
+import { FACES } from '/lib/constants.js'
+
+define('dice-root', (el) => {
+  render(el, renderComponent)
+})
+
+function renderComponent () {
+  return html`
+    ${FACES.map((faces) => renderButton({ faces }))}
+  `
+}
+
+function renderButton (props) {
+  const { faces } = props
+  return html`
+    <button
+      faces=${faces}
+      is='dice-button'
+      label='1'
+      size='medium'
+      theme='solid'>
+    </button>
+  `
+}
